ab8064c8c2f77b3f413eb107f1239f0c701a81fe
taufiq <taufiqmmhd@gmail.com>
2023-12-29 18:26:16 +0800

    Initial Commit

---
+++ CMakeFiles/chippy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chippy.dir/main.c.o"
+  "CMakeFiles/chippy.dir/main.c.o.d"
+  "chippy"
+  "chippy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chippy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
