25fc5beed924020bef9e8c1f882de3dec1c018f2
taufiq <taufiqmmhd@gmail.com>
2025-06-13 18:43:56 +0800

    fix: remove unused override function

--- Box.h
+++ Box.h
@@ -21,7 +21,6 @@ namespace UI
         void render(SDL_Renderer *renderer, TextManager *textManager, Context ctx) override;
         void measure(TextManager *textManager, float availableWidth, float availableHeight) override;
         void layoutChildrenEvenly(int i, int j, float startingX, float startingY, float availableWidth);
-        void setBounds(Bounds _bounds) override;
 
         void setPaddingX(int _p) { style.paddingX = _p; };
     };
